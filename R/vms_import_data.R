0d4d2c5d9af8cdf8a678fbbb702b88da3c933275
einar.hjorleifsson <einar.hjorleifsson@gmail.com>
2018-06-07 19:41:34 +0000

    testing anytime::anytime to guess date format. not very efficient (is slow). in addition anytime("June 6, 2018 12:43:00 PM") does not work - a format is some liberian files

--- R/vms_import_data.R
+++ R/vms_import_data.R
@@ -4,100 +4,80 @@
 #'
 #' @param file.name XXX
 #' @param country XXX
-#' @param lon.min XXX
-#' @param lon.max XXX
-#' @param lat.min XXX
-#' @param lat.max XXX
 #'
 #' @return dataframe
 #' @export
 #'
-vms_import_data <- function(file.name, country,
-                            lon.min,
-                            lon.max,
-                            lat.min,
-                            lat.max)
+vms_import_data <- function(file.name, country)
 {
+
+  if(!country %in% c("Iceland", "Ghana", "Liberia", "Sierra Leone"))
+    stop('The only vms country formats supported so far are: "Iceland", "Ghana", "Liberia", "Sierra Leone"')
+
+
+  d <-
+    rio::import(file.name, setclass="tibble") %>%
+    dplyr::rename_all(., tolower)
+  colnames(d) <- stringr::str_trim(colnames(d))
+  colnames(d) <- vms_standardize_names(colnames(d))
+
+  if(class(d$lon) == "character")   d$lon <-   as.numeric(stringr::str_replace(d$lon, ",", "."))
+  if(class(d$lat) == "character")   d$lat <-   as.numeric(stringr::str_replace(d$lat, ",", "."))
+  if(class(d$speed) == "character") d$speed <- as.numeric(stringr::str_replace(d$speed, ",", "."))
+
+  if(!any(colnames(d) %in% "vid")) {
+    d$vid <- file.name
+  }
+  if(class(d$vid) != "character")   d$vid <-   as.character(d$vid)
+
 
   if(country == "Iceland") {
     d <-
-      readr::read_csv(file.name) %>%
-      dplyr::rename(lon = poslon,
-                    lat = poslat,
-                    vid = mobileid,
-                    date = posdate) %>%
+      d %>%
       dplyr::mutate(lon = lon * 45 / atan(1),
                     lat = lat * 45 / atan(1),
                     heading = heading * 45 / atan(1),
-                    speed = speed * 1.852) %>%
-      dplyr::arrange(vid, date) %>%
-      dplyr::mutate(activity = dplyr::case_when(speed < speed.min ~ paste0("<", speed.min),
-                                                speed < speed.max ~ paste0(speed.min, "-", speed.max),
-                                                TRUE ~ paste0(">", speed.max)))
+                    speed = speed * 1.852)
+                    #time = lubridate::ymd_hms(time),
+                    #recdate = lubridate::ymd_hms(recdate),
+                    #vid = as.character(vid)) %>%
+      #dplyr::arrange(vid, time)
   }
 
   if(country == "Ghana") {
-
     d <-
-      rio::import(file.name, setclass="tibble") %>%
-      dplyr::rename(vid = Mobile,
-                    date = Date,
-                    lon = Longitude,
-                    lat = Latitude,
-                    speed = Speed) %>%
-      dplyr::mutate(date = lubridate::dmy_hm(date),
-                    lon = as.numeric(lon),
-                    lat = as.numeric(lat),
-                    speed = as.numeric(speed),
-                    activity = dplyr::case_when(speed < speed.min ~ paste0("<", speed.min),
-                                                speed < speed.max ~ paste0(speed.min, "-", speed.max),
-                                                TRUE ~ paste0(">", speed.max))) %>%
-      dplyr::arrange(vid, date)
+      d #%>%
+      #dplyr::mutate(#time = lubridate::dmy_hm(time),
+      #              lon = as.numeric(lon),
+      #              lat = as.numeric(lat),
+      #              speed = as.numeric(speed))
   }
 
   if(country == "Sierra Leone") {
-    d <- rio::import(file.name, setclass="tibble") %>%
-      dplyr::rename(vid = PublicDeviceID,
-                    date = gpsTime, # Check
-                    lon = Lon,
-                    lat = Lat,
-                    speed = Speed) %>%
-      dplyr::mutate(lat = as.numeric(stringr::str_replace(lat,  ",",  ".")),
-                    lon = as.numeric(stringr::str_replace(lon,  ",",  ".")),
-                    date = stringr::str_sub(date, 1, 19),
-                    date = lubridate::ymd_hms(date),
-                    speed = as.numeric(speed) * 3600/185200,
-                    activity = dplyr::case_when(speed < speed.min ~ paste0("<", speed.min),
-                                                speed < speed.max ~ paste0(speed.min, "-", speed.max),
-                                                TRUE ~ paste0(">", speed.max)))%>%
-      dplyr::arrange(vid, date)
+    d <-
+      d %>%
+      dplyr::mutate(#lat = as.numeric(stringr::str_replace(lat,  ",",  ".")),
+                    #lon = as.numeric(stringr::str_replace(lon,  ",",  ".")),
+                    time = stringr::str_sub(time, 1, 19),
+                    #time = lubridate::ymd_hms(time),
+                    speed = speed * 3600/185200)
   }
 
   if(country == "Liberia") {
     d <-
-      rio::import(file.name, setclass="tibble") %>%
-      dplyr::rename(date = DateTime, # or gpsTime???
-                    lon = Longitude,
-                    lat = Latitude,
-                    speed = "Speed (MPH)") %>%
-      dplyr::mutate(date = lubridate::ymd_hms(date),
-                    lat = as.numeric(lat),
-                    lon = as.numeric(lon),
-                    activity = dplyr::case_when(speed < speed.min ~ paste0("<", speed.min),
-                                                speed < speed.max ~ paste0(speed.min, "-", speed.max),
-                                                TRUE ~ paste0(">", speed.max))) %>%
-      dplyr::arrange(vid, date)
+      d #%>%
+      #dplyr::mutate(#time = lubridate::ymd_hms(time),
+      #              lat = as.numeric(lat),
+      #              lon = as.numeric(lon))
   }
 
 
   d <-
     d %>%
-    dplyr::filter(dplyr::between(lon, -179.99999999999, 179.99999999999),
-                  dplyr::between(lat, -89.99999999999, 89.99999999999)) %>%
-    dplyr::mutate(fishing = ifelse(dplyr::between(speed, speed.min, speed.max),
-                                   TRUE, FALSE),
-                  country = country)
-
+    #dplyr::filter(dplyr::between(lon, -179.99999999999, 179.99999999999),
+    #              dplyr::between(lat, -89.99999999999, 89.99999999999)) %>%
+    dplyr::mutate(time2 = anytime::anytime(time)) #%>%
+    #arrange(vid, time2)
 
   return(d)
 }
---
+++ R/vms_standardize_names.R
@@ -0,0 +1,55 @@
+# Column names in vms files ----------------------------------------------------
+
+# Ghana ------------------------------------------------------------------------
+# Vessel Name, Date, Lat, Lond, Speed, EEZ
+# Mobile, Date, Latitude, Longitude, Speed
+# Mobile;Date;Latitude;Longitude;Speed;EEZ1;Port;Radio Call Sign;MMSI
+
+# Sierra Leone -----------------------------------------------------------------
+# ReceiveTime;gpsTime;Lat;Lon;Speed;gpsQuality;PublicDeviceID;Type
+
+# Liberia ----------------------------------------------------------------------
+# "vessel","datetime","lon","lat","dist_Nm","speed_kts"
+# Date/Time	Latitude	Longitude	Event Type	Speed (MPH)	Heading
+# Date/Time	Latitude	Longitude	Event Type	Speed (MPH)	Heading
+
+
+#' Title
+#'
+#' @param cn A character vector
+#'
+#' @return A character vector
+#' @export
+#'
+vms_standardize_names <- function(cn) {
+
+  cn <- stringr::str_trim(cn)
+
+  cn.time <- c("time", "date", "gpstime", "date/time", "posdate", "datetime")
+  cn.lon <- c("lon", "lond", "longitude", "poslon")
+  cn.lat <- c("lat", "latitude", "poslat")
+  cn.speed <- c("speed", "speed_kts", "speed (mph)")
+  cn.vid <- c("vid", "mobileid", "vessel name", "mobile", "publicdeviceid", "vessel", "vessel name")
+  cn.eez <- c("eez", "eez1")
+
+  i <- cn %in% cn.time
+  cn[i] <- "time"
+
+  i <- cn %in% cn.lon
+  cn[i] <- "lon"
+
+  i <- cn %in% cn.lat
+  cn[i] <- "lat"
+
+  i <- cn %in% cn.speed
+  cn[i] <- "speed"
+
+  i <- cn %in% cn.vid
+  cn[i] <- "vid"
+
+  i <- cn %in% cn.eez
+  cn[i] <- "eez"
+
+  return(cn)
+
+}
